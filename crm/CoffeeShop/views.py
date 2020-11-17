0757abf934fd151235a526c6f0b8db5558e58f2a
Manash Mondal <emailatmanash@gmail.com>
2020-11-17 21:24:01 +0600

    template setup| basic jinja setup

--- crm/CoffeeShop/views.py
+++ crm/CoffeeShop/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    # return HttpResponse("Home Page")
+    return render(request, 'CoffeeShop/dashboard.html')
+
+def products(request):
+    return render(request, 'CoffeeShop/products.html')
+
+def customers(request):
+    return render(request, 'CoffeeShop/customers.html')
